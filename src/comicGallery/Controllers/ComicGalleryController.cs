1646a1507f29d647e78f873144d867675d654d95
FeedTheMind <FeedTheMind@users.noreply.github.com>
2017-02-16 09:58:43 -0500

    Update Detail to use _Layout

--- src/comicGallery/Controllers/ComicGalleryController.cs
+++ src/comicGallery/Controllers/ComicGalleryController.cs
@@ -10,6 +10,19 @@ namespace comicGallery.Controllers
     {
         public ActionResult Detail() // "action" method
         {
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his greatest act of revenge! Even if Spider-Man survives . . . <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();
         }
     }
@@ -21,5 +34,6 @@ namespace comicGallery.Controllers
  1) Other classes can accept "public" modifiers    
  2) Common to add "controllers" to Controllers folder
  3) "action" method performs actions to prepare response   
-     
+ 4) Convention . . . views in Views folder
+    Filename should match action method
 */
