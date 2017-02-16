464a43dda6ee6e858814a1e08f61c37d4eb0bed3
FeedTheMind <FeedTheMind@users.noreply.github.com>
2017-02-16 07:55:34 -0500

    Add empty MVC, ComicGallery controller, and Detail view

---
+++ src/comicGallery/Controllers/ComicGalleryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace comicGallery.Controllers
+{
+    public class ComicGalleryController : Controller
+    {
+        public ActionResult Detail() // "action" method
+        {
+            return View();
+        }
+    }
+}
+
+/* 
+ 
+ Notes:
+ 1) Other classes can accept "public" modifiers    
+ 2) Common to add "controllers" to Controllers folder
+ 3) "action" method performs actions to prepare response   
+     
+*/
