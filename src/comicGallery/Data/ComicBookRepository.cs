9b3611307c5f01e4fbf99534d3c2d906c8cb4b77
FeedTheMind <FeedTheMind@users.noreply.github.com>
2017-02-18 10:28:59 -0500

    Use foreach and update controller value

--- src/comicGallery/Data/ComicBookRepository.cs
+++ src/comicGallery/Data/ComicBookRepository.cs
@@ -57,6 +57,11 @@ namespace comicGallery.Data
             }
         };
 
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
